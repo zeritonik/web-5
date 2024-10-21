31c35d55de0d84e9e74dea03f00f6f57be201989
zeritonik <zaq31337@gmail.com>
2024-10-21 12:24:35 +0300

    solved work

--- projects/work/main.go
+++ projects/work/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
-	// "sync"
 )
 
 func work() {
@@ -13,4 +13,13 @@ func work() {
 
 func main() {
 	// необходимо в отдельных горутинах вызвать функцию work() 10 раз и дождаться результатов выполнения вызванных функций
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			work()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
