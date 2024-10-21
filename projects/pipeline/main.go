270d6aeb0e42de7a69b3b78aac59e11217c4d2dc
zeritonik <zaq31337@gmail.com>
2024-10-21 12:18:53 +0300

    solved pipeline

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,34 @@
 package main
 
+import "fmt"
+
 // реализовать removeDuplicates(in, out chan string)
+func removeDuplicates(in, out chan string) {
+	prev := ""
+	for val := range in {
+		if val != prev {
+			out <- val
+			prev = val
+		}
+	}
+	close(out)
+}
 
 func main() {
 	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	in, out := make(chan string), make(chan string)
+
+	go func() {
+		values := [...]string{"123", "321", "321", "111"}
+		for val := range values {
+			in <- values[val]
+		}
+		close(in)
+	}()
+
+	go removeDuplicates(in, out)
+
+	for val := range out {
+		fmt.Println(val)
+	}
 }
